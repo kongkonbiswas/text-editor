30ccb38d1776eab16274ba02cb8eccd6c85e888a
Kongkon Biswas <kongkonbiswas3241@gmail.com>
2022-04-04 23:55:57 +0600

    some additional change

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import TextEditor from './components/TextEditor';
+
+   
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TextEditor from "./components/TextEditor";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>React Text Editor</h1>
-
       </header>
-      <div className='editor'>
-        <TextEditor></TextEditor>
+      <div className="editor">
+        <TextEditor />
       </div>
     </div>
   );
